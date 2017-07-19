01136bd2a7943fe6a6a8250245b54a086a837278
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    [SQUASH] base: Allow using 4G icon instead of LTE [1/2]
    [jhonboy121]: adapt for A13
    
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    2nd commit:
    
    SystemUI: improve 4G icon switch code
    
    * adapted to A12 L and A13
    * added setting key to backup array
    * use a single settings observer and use existing mReceiverHandler instead of creating new one's
    * keep the settings observer class private
    * don't update configuration when registering observer
    * only notify listeners if necessary
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    3rd commit:
    
    SystemUI: MobileSignalController: register 4G icon settings observer for all users
    
    * update settings on user switch as well
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,5 +92,6 @@ public class SystemSettings {
         Settings.System.QQS_SHOW_BRIGHTNESS,
         Settings.System.SHOW_AUTO_BRIGHTNESS_BUTTON,
         Settings.System.BRIGHTNESS_SLIDER_POSITION,
+        Settings.System.SHOW_FOURG_ICON,
     };
 }
