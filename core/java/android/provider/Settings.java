55305933bda3eb5c6c73de903fdfd7f64b6ac5d3
Jyotiraditya <jyotiraditya@aospa.co>
2020-10-30 11:30:59 -0300

    SystemUI: Introduce Adaptive Playback [1/2]
    Automatically pause media when muted. Resume if unmuted within a minute.
    
    Co-Authored-By: Stylogey <stylogey@gmail.com>
    Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5607,6 +5607,22 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
+        /**
+         * Adaptive playback
+         * Automatically pause media when the volume is muted and
+         * will resume automatically when volume is restored.
+         *   0 = disabled
+         *   1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+        /**
+         * Adaptive playback's timeout in ms
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
+
        /**
          * Whether or not volume button music controls should be enabled to seek media tracks
          * @hide
