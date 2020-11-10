1aa38df353725840fdd085f40ffb26190003810b
Keith Mok <kmok@cyngn.com>
2020-11-10 10:47:09 -0300

    SystemUI: Add caffeine QS tile
    * Caffeine Tile icon designed by Andrew Fluck
    
    [Adin Kwok] Add long press to infinity to caffeine qs tile
    
    [ghostrider-reborn] Updated for android 12
    
    Co-authored-by: Adin Kwok <adin@adin.dev>
    Change-Id: If54ea27ddf179c2de39b61850163e14173727698
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -52,6 +52,7 @@ import com.android.systemui.flags.FlagsModule;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyguard.data.BouncerViewModule;
+import com.android.systemui.reloaded.ReloadedModule;
 import com.android.systemui.log.dagger.LogModule;
 import com.android.systemui.mediaprojection.appselector.MediaProjectionModule;
 import com.android.systemui.model.SysUiState;
@@ -167,6 +168,7 @@ import dagger.Provides;
             FooterActionsModule.class,
             GarbageMonitorModule.class,
             KeyboardModule.class,
+            ReloadedModule.class,
             LogModule.class,
             MediaProjectionModule.class,
             MotionToolModule.class,
