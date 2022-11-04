28c8c583b70a2d520b5620841ab635e328697f24
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-04 19:30:55 +0530

    base: Switch to PixelPropsUtils
    Play Protect was broken (uncertified device) on Realme X3.
    Reported to be broken on few other devices too, in other
    ROMs using PropImitationHooks. What's the point of having
    a clean implementation if it doesn't fucking work?
    
    Imported PPU from PE 13. Full history:
    https://github.com/PixelExperience/frameworks_base/commits/8a940b5453ffd5adc9775f60b4b9b69662fe8e0d/core/java/com/android/internal/util/custom/PixelPropsUtils.java
    
    Massively cleaned up code.
    
    As an added bonus, we also get the new Material You UI
    for the GMS bits in setup wizard.
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,7 +56,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.PixelPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1244,7 +1244,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PropImitationHooks.setProps(app);
+        PixelPropsUtils.setProps(app);
         return app;
     }
     
@@ -1262,7 +1262,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PropImitationHooks.setProps(app);
+        PixelPropsUtils.setProps(app);
         return app;
     }
 
