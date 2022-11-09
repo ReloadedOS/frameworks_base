AOSPA/android_frameworks_base-old

commit 1b6e12f01dd6a17bf6d7e3aef08e95e38116f641

SystemUI: Show notification row when it's unliked to ringer volume
Signed-off-by: Mesquita <mesquita@aospa.co>
Change-Id: Id7a2602153dbc94eb40caf70946d2b31e4a3c19f

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1622,6 +1622,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
+                        || row.stream == STREAM_NOTIFICATION
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }

ReloadedOS/frameworks_base

commit 5b14da91a952c2eaee8dc0cb6c83658bbc1b03ac

SystemUI: Show notification row when it's unlinked to ringer volume
Squashed:

SystemUI: Show notification row only when unlinked

Commit 1b6e12f01dd6a17bf6d7e3aef08e95e38116f641
("SystemUI: Show notification row when it's unliked to ringer volume")
caused notification row to show in expanded volume panel
even when notification and ring volumes are linked. Fix
it so that it shows only when they are unlinked.

Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: Id7a2602153dbc94eb40caf70946d2b31e4a3c19f

packages/SystemUI/plugin/src/com/android/systemui/plugins/VolumeDialogController.java
@@ -106,7 +106,7 @@ public interface VolumeDialogController {
         public ComponentName effectsSuppressor;
         public String effectsSuppressorName;
         public int activeStream = NO_ACTIVE_STREAM;
-        public boolean linkedNotification;
+        public boolean linkedNotification = true;
         public boolean disallowAlarms;
         public boolean disallowMedia;
         public boolean disallowSystem;

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -215,6 +215,8 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
                     VolumePolicy.A11Y_MODE_INDEPENDENT_A11Y_VOLUME :
                         VolumePolicy.A11Y_MODE_MEDIA_A11Y_VOLUME);
 
+        updateLinkNotificationConfigW();
+
         mWakefulnessLifecycle.addObserver(mWakefullnessLifecycleObserver);
     }
 

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1654,6 +1654,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
+                        || (row.stream == STREAM_NOTIFICATION && !mState.linkedNotification)
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }