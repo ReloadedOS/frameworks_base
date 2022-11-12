ReloadedOS/frameworks_base

commit 76a37de90d47181ded3c8883f0f99e574f6f14d6

SystemUI: Double tap to sleep on statusbar [1/2]
arrow-11.0 edits:
- Adapt code to refactored statusbar controllers.

wave edits:
- Adapt to existing dt2s on lockscreen

commit f3321f6a498b3b55a509524183ce83b7503cf936
Author: maxwen <max.weninger@gmail.com>
Date:   Wed May 23 21:55:04 2018 +0200

    base: SystemUI: prevent that double tap to sleep triggers panel expand

    arrow-11.0 edits:
        - Adapt code to refactored statusbar controllers.

    http://jira.omnirom.org/browse/OMNI-1466

    Change-Id: If304577d666ebf8423cca1298dfbe044843657cc

Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045

[ghostrider-reborn | imjyotiraditya] adapted to android 12

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: Ibfcd4619654910a95e6d49acbbceb15e21e2d2d0

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -718,6 +718,8 @@ public class NotificationPanelViewController extends PanelViewController {
         }
     };
 
+    private int mStatusBarHeaderHeight;
+
     @Inject
     public NotificationPanelViewController(NotificationPanelView view,
             @Main Resources resources,
@@ -1104,6 +1106,8 @@ public class NotificationPanelViewController extends PanelViewController {
         mLockscreenNotificationQSPadding = mResources.getDimensionPixelSize(
                 R.dimen.notification_side_paddings);
         mUdfpsMaxYBurnInOffset = mResources.getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+        mStatusBarHeaderHeight = mResources.getDimensionPixelSize(
+                R.dimen.status_bar_height);
     }
 
     private void updateViewControllers(KeyguardStatusView keyguardStatusView,
@@ -4255,6 +4259,15 @@ public class NotificationPanelViewController extends PanelViewController {
                         Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
                         Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1) {
                     mDoubleTapGestureListener.onTouchEvent(event);
+                } else if (!mQsExpanded
+                        && mDoubleTapToSleepEnabled
+                        && event.getY() < mStatusBarHeaderHeight) {
+                    mDoubleTapGestureListener.onTouchEvent(event);
+                    // quick pulldown can trigger those values
+                    // on double tap - so reset them
+                    mQsExpandImmediate = false;
+                    requestPanelHeightUpdate();
+                    setListening(false);
                 }
 
                 // Make sure the next touch won't the blocked after the current ends.
@@ -5211,6 +5224,10 @@ public class NotificationPanelViewController extends PanelViewController {
                     false /* delayed */,
                     1.0f /* speedUpFactor */);
         }
+    }
+
+    public void updateDoubleTapToSleep(boolean doubleTapToSleepEnabled) {
+        mDoubleTapToSleepEnabled = doubleTapToSleepEnabled;
     }
 
     @SysUISingleton

core/java/android/provider/Settings.java
@@ -5590,6 +5590,12 @@ public final class Settings {
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
         /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5713,6 +5719,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE_VENDOR_HINT);
             PRIVATE_SETTINGS.add(CALL_CONNECTED_TONE_ENABLED);
+            PRIVATE_SETTINGS.add(DOUBLE_TAP_SLEEP_GESTURE);
         }
 
         /**

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -93,5 +93,6 @@ public class SystemSettings {
         Settings.System.SHOW_AUTO_BRIGHTNESS_BUTTON,
         Settings.System.BRIGHTNESS_SLIDER_POSITION,
         Settings.System.SHOW_FOURG_ICON,
+        Settings.System.DOUBLE_TAP_SLEEP_GESTURE
     };
 }

packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -212,5 +212,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.SHOW_AUTO_BRIGHTNESS_BUTTON, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.BRIGHTNESS_SLIDER_POSITION, new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(System.SHOW_FOURG_ICON, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.DOUBLE_TAP_SLEEP_GESTURE, BOOLEAN_VALIDATOR);
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/LockscreenShadeTransitionController.kt
@@ -6,6 +6,7 @@ import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.Configuration
+import android.os.PowerManager
 import android.os.SystemClock
 import android.util.IndentingPrintWriter
 import android.util.MathUtils
@@ -729,8 +730,13 @@ class DragDownHelper(
     private var dragDownAmountOnStart = 0.0f
     lateinit var expandCallback: ExpandHelper.Callback
     lateinit var host: View
+    lateinit var goToSleep: Runnable
 
     private var minDragDistance = 0
+    private var doubleTapToSleepEnabled = false
+    private var statusBarHeaderHeight: Int = 0
+    private var lastDownEvent: Long = -1
+    private var doubleTapTimeout: Long = 0
     private var initialTouchX = 0f
     private var initialTouchY = 0f
     private var touchSlop = 0f
@@ -764,6 +770,13 @@ class DragDownHelper(
         val configuration = ViewConfiguration.get(context)
         touchSlop = configuration.scaledTouchSlop.toFloat()
         slopMultiplier = configuration.scaledAmbiguousGestureMultiplier
+        doubleTapTimeout = ViewConfiguration.getDoubleTapTimeout().toLong()
+        statusBarHeaderHeight = context
+            .resources.getDimensionPixelSize(R.dimen.status_bar_header_height_keyguard)
+        goToSleep = Runnable {
+            val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+            pm.goToSleep(lastDownEvent)
+        }
     }
 
     override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
@@ -776,6 +789,18 @@ class DragDownHelper(
                 startingChild = null
                 initialTouchY = y
                 initialTouchX = x
+                if (doubleTapToSleepEnabled && y < statusBarHeaderHeight) {
+                    val eventTime = event.eventTime
+                    lastDownEvent = if (lastDownEvent != -1L) {
+                        val diff = eventTime - lastDownEvent
+                        if (diff < doubleTapTimeout) {
+                            goToSleep.run()
+                        }
+                        -1
+                    } else {
+                        eventTime
+                    }
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 val h = y - initialTouchY
@@ -910,5 +935,9 @@ class DragDownHelper(
     private fun findView(x: Float, y: Float): ExpandableView? {
         host.getLocationOnScreen(temp2)
         return expandCallback.getChildAtRawPosition(x + temp2[0], y + temp2[1])
+    }
+
+    public fun updateDoubleTapToSleep(updateDoubleTapToSleep: Boolean) {
+        doubleTapToSleepEnabled = updateDoubleTapToSleep
     }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -67,6 +67,7 @@ import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.hardware.devicestate.DeviceStateManager;
@@ -681,6 +682,30 @@ public class CentralSurfacesImpl extends CoreStartable implements
 
     private final InteractionJankMonitor mJankMonitor;
 
+    private class CustomSettingsObserver extends ContentObserver {
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        public void update() {
+            if (mNotificationShadeWindowViewController != null) {
+                mNotificationShadeWindowViewController.updateSettings();
+            }
+        }
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver;
 
     /**
      * Public constructor for CentralSurfaces.
@@ -1120,6 +1145,10 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 (requestTopUi, componentTag) -> mMainExecutor.execute(() ->
                         mNotificationShadeWindowController.setRequestTopUi(
                                 requestTopUi, componentTag))));
+
+        mCustomSettingsObserver = new CustomSettingsObserver(mMainHandler);
+        mCustomSettingsObserver.observe();
+        mCustomSettingsObserver.update();
     }
 
     private void onFoldedStateChanged(boolean isFolded, boolean willGoToSleep) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationShadeWindowViewController.java
@@ -503,4 +503,16 @@ public class NotificationShadeWindowViewController {
     void setDragDownHelper(DragDownHelper dragDownHelper) {
         mDragDownHelper = dragDownHelper;
     }
+
+    public void updateSettings() {
+        boolean doubleTapToSleepEnabled = Settings.System.getIntForUser(
+                mView.getContext().getContentResolver(), Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mNotificationPanelViewController != null) {
+            mNotificationPanelViewController.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+        if (mDragDownHelper != null) {
+            mDragDownHelper.updateDoubleTapToSleep(doubleTapToSleepEnabled);
+        }
+    }
 }

packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelViewController.java
@@ -202,6 +202,8 @@ public abstract class PanelViewController {
     private final InteractionJankMonitor mInteractionJankMonitor;
     protected final SystemClock mSystemClock;
 
+    protected boolean mDoubleTapToSleepEnabled;
+
     protected abstract void onExpandingFinished();
 
     protected void onExpandingStarted() {
@@ -1373,7 +1375,7 @@ public abstract class PanelViewController {
                         onTrackingStarted();
                     }
                     if (isFullyCollapsed() && !mHeadsUpManager.hasPinnedHeadsUp()
-                            && !mCentralSurfaces.isBouncerShowing()) {
+                            && !mCentralSurfaces.isBouncerShowing() && !mDoubleTapToSleepEnabled) {
                         startOpening(event);
                     }
                     break;

commit 02312b6035bd6fef816981fed4c9069c7f9db237

SystemUI: Fix statusbar expansion with DT2S enabled
Do not always block immediate expansion when status bar
DT2S is enabled. This is required for the subtle tick
vibration when expanding notification shade.

Use pre-defined power manager while we're at it.

Fixes: 76a37de90d47181ded3c8883f0f99e574f6f14d6 ("Double tap to sleep on statusbar")

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -893,16 +893,21 @@ public class NotificationPanelViewController extends PanelViewController {
         });
         mBottomAreaShadeAlphaAnimator.setDuration(160);
         mBottomAreaShadeAlphaAnimator.setInterpolator(Interpolators.ALPHA_OUT);
+
         mDoubleTapGestureListener = new GestureDetector(mView.getContext(),
                 new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onDoubleTap(MotionEvent event) {
-                final PowerManager pm = (PowerManager) mView.getContext().getSystemService(
-                        Context.POWER_SERVICE);
-                pm.goToSleep(event.getEventTime());
+                // quick pulldown can trigger those values
+                // on double tap - so reset them
+                mQsExpandImmediate = false;
+                requestPanelHeightUpdate();
+                setListening(false);
+                mPowerManager.goToSleep(event.getEventTime());
                 return true;
             }
         });
+
         mEntryManager = notificationEntryManager;
         mConversationNotificationManager = conversationNotificationManager;
         mAuthController = authController;
@@ -4264,19 +4269,14 @@ public class NotificationPanelViewController extends PanelViewController {
                     return false;
                 }
 
-                if (mBarState == StatusBarState.KEYGUARD && !mPulsing && !mDozing &&
+                // Double tap to sleep on keyguard
+                if ((mBarState == StatusBarState.KEYGUARD && !mPulsing && !mDozing &&
                         Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
-                        Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1) {
+                        Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1)
+                        // Double tap to sleep on statusbar
+                        || (mDoubleTapToSleepEnabled && !mQsExpanded
+                        && event.getY() < mStatusBarHeaderHeight)) {
                     mDoubleTapGestureListener.onTouchEvent(event);
-                } else if (!mQsExpanded
-                        && mDoubleTapToSleepEnabled
-                        && event.getY() < mStatusBarHeaderHeight) {
-                    mDoubleTapGestureListener.onTouchEvent(event);
-                    // quick pulldown can trigger those values
-                    // on double tap - so reset them
-                    mQsExpandImmediate = false;
-                    requestPanelHeightUpdate();
-                    setListening(false);
                 }
 
                 // Make sure the next touch won't the blocked after the current ends.