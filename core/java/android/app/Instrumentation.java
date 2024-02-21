f48416fd814587ece2c65183073851825ee4cf0c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-02-21 18:10:51 +0530

    PixelPropsUtils: make certification props for spoof updatable via overlay
    Change-Id: Icf60a9d49c5234c30ec4ab10af2b51c96851d690
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1284,8 +1284,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(context);
         return app;
     }
     
@@ -1303,8 +1302,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        String packageName = context.getPackageName();
-        PixelPropsUtils.setProps(packageName);
+        PixelPropsUtils.setProps(context);
         return app;
     }
 
